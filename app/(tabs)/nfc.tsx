fddedeccf2b30b3c8f626b1eb393551b16ddbe3f
Diego Perez <diego.fernando.perez.luna@gmail.com>
2026-04-14 15:25:12 -0400

    Add: nfc service

---
+++ app/(tabs)/nfc.tsx
@@ -0,0 +1,54 @@
+import { StyleSheet, View } from 'react-native';
+
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { IconSymbol } from '@/components/ui/icon-symbol';
+import { Fonts } from '@/constants/theme';
+
+export default function TabTwoScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerImage={
+        <IconSymbol
+          size={310}
+          color="#808080"
+          name="chevron.left.forwardslash.chevron.right"
+          style={styles.headerImage}
+        />
+      }
+    >
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText
+          type="title"
+          style={{
+            fontFamily: Fonts.rounded,
+          }}
+        >
+          NFC
+        </ThemedText>
+          <View style={styles.content}>
+            <ThemedText>skere</ThemedText>
+          </View>
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'column',
+    gap: 8,
+  },
+  content: {
+    padding: 16,
+    borderRadius: 8,
+  },
+});
---
+++ src/services/nfc-service.ts
@@ -0,0 +1,31 @@
+import NfcManager, { Ndef, NfcTech } from 'react-native-nfc-manager';
+
+export const nfcService = {
+    init: async () => {
+        await NfcManager.start();
+    },
+
+    writeProductId: async (uniqueId: string) => {
+        let result = false;
+
+        try {
+            await NfcManager.requestTechnology(NfcTech.Ndef);
+
+            const bytes = Ndef.encodeMessage([
+                Ndef.textRecord(uniqueId),
+            ])
+
+            if (bytes) {
+                await NfcManager.ndefHandler.writeNdefMessage(bytes);
+                result = true;
+            }
+        } catch (error) {
+            console.error('Error writing NFC data:', error);
+
+        } finally {
+            await NfcManager.cancelTechnologyRequest();
+        }
+
+        return result;
+    }
+}
