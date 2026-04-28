a80a36804479a7c1115046ae57624cc8dd36d3f6
Diego <diego.fernando.perez.luna@gmail.com>
2026-04-28 18:04:54 -0400

    Add: nfc finished

--- src/services/nfc-service.ts
+++ src/services/nfc-service.ts
@@ -1,34 +1,54 @@
 import NfcManager, { Ndef, NfcTech } from 'react-native-nfc-manager';
 
 export const nfcService = {
-    init: async () => {
-        const isSupported = await NfcManager.isSupported();
-        if (!isSupported) throw new Error('NFC no soportado en este dispositivo');
+  init: async () => {
+    const isSupported = await NfcManager.isSupported();
+    if (!isSupported) throw new Error('NFC no soportado en este dispositivo');
 
-        await NfcManager.start();
-    },
+    await NfcManager.start();
+  },
 
-    writeProductId: async (uniqueId: string) => {
-        let result = false;
+  writeProductId: async (uniqueId: string) => {
+    let result = false;
 
-        try {
-            await NfcManager.requestTechnology(NfcTech.Ndef);
+    try {
+      await NfcManager.requestTechnology([
+        NfcTech.Ndef,
+        NfcTech.NdefFormatable,
+      ]);
 
-            const bytes = Ndef.encodeMessage([
-                Ndef.textRecord(uniqueId),
-            ])
+      const tag = await NfcManager.getTag();
+      console.log('Tecnologías detectadas:', tag?.techTypes);
 
-            if (bytes) {
-                await NfcManager.ndefHandler.writeNdefMessage(bytes);
-                result = true;
-            }
-        } catch (error) {
-            console.error('Error writing NFC data:', error);
+      const bytes = Ndef.encodeMessage([Ndef.textRecord(uniqueId)]);
 
-        } finally {
-            await NfcManager.cancelTechnologyRequest();
-        }
+      const hasNdef = tag?.techTypes?.some(
+        (type) => type.includes('Ndef') && !type.includes('NdefFormatable'),
+      );
+      const hasNdefFormatable = tag?.techTypes?.some((type) =>
+        type.includes('NdefFormatable'),
+      );
 
-        return result;
+      if (hasNdef) {
+        console.log('Escribiendo en modo NDEF directo...');
+        await NfcManager.ndefHandler.writeNdefMessage(bytes);
+        result = true;
+      } else if (hasNdefFormatable) {
+        console.log('Formateando y escribiendo en modo NdefFormatable...');
+        await NfcManager.ndefFormatableHandlerAndroid.formatNdef(bytes);
+        result = true;
+      } else {
+        console.warn(
+          'El tag no parece ser compatible con escritura NDEF estándar.',
+        );
+      }
+    } catch (error) {
+      console.warn('Error durante la escritura:', error);
+    } finally {
+      await NfcManager.cancelTechnologyRequest();
     }
-}
+
+    console.log('resultado:', result);
+    return result;
+  },
+};
